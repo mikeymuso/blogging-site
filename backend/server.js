7ef0a28e60c527e6abdfb17fe76619c301afbefc
Mike Cotton <mick_cotton@hotmail.com>
2021-02-08 16:36:44 +0000

    setup for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import path from 'path';
 import colors from 'colors';
 
 // ===========================
@@ -26,14 +27,24 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('The server is working.');
-});
-
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/comments', commentRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('The server is working.');
+  });
+}
 
 app.use(function (err, req, res, next) {
   console.error(err.stack.yellow);
